03739c777efc7e791b94373464d9f337c0db5ae1
Fatih Baser <basese_aehit@students.vizja.pl>
2022-03-23 15:39:57 +0100

    arranged packages

--- app/src/main/java/com/fatihbaserpl/foodapplication/ui/MainActivity.kt
+++ app/src/main/java/com/fatihbaserpl/foodapplication/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.fatihbaserpl.foodapplication
+package com.fatihbaserpl.foodapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.fatihbaserpl.foodapplication.R
 import com.fatihbaserpl.foodapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/fatihbaserpl/foodapplication/ui/fragments/RecipesFragment.kt
+++ app/src/main/java/com/fatihbaserpl/foodapplication/ui/fragments/RecipesFragment.kt
@@ -1,10 +1,11 @@
-package com.fatihbaserpl.foodapplication
+package com.fatihbaserpl.foodapplication.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fatihbaserpl.foodapplication.R
 
 
 class RecipesFragment : Fragment() {
